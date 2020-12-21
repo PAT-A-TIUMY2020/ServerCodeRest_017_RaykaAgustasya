54975a90be6111e94e3abcd9060cddfccac11dab
Rayka Agustasya <raykaagustasya16@gmail.com>
2020-12-21 21:40:11 +0700

    Add project files.

---
+++ ServerCodeRest_20180140017_RaykaAgustasya/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCodeRest_20180140017_RaykaAgustasya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
